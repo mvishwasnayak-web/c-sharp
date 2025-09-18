7afa64feea63055daf536e5bdd8dda9c9f923823
mvishwasnayak-web <mvishwasnayak@gmail.com>
2025-09-18 10:36:35 +0530

    Add files via upload

---
+++ Nestedtry/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter a divisor");
+        int divisor = Convert.ToInt32(Console.ReadLine());  
+        try
+        {
+            try
+            {
+                int result = 10 / divisor;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("inner catch is executed:" + e.Message);
+            }
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine("outer catch is executed:" + e.Message);
+        }
+        Console.ReadLine();
+    }
+}
+
+
