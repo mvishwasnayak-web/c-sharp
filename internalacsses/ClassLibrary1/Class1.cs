7dc1d77068adcb1f089e5663eb7012445ef5f300
mvishwasnayak-web <mvishwasnayak@gmail.com>
2025-09-18 10:35:31 +0530

    Add files via upload

---
+++ internalacsses/ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;   
+
+namespace ClassLibrary1
+{
+    public class point
+    {
+        public void Display()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ internalacsses/internalacsses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using ClassLibrary1;
+namespace C
+{
+    class point
+    {
+        internal int x;
+        internal int y;
+        public void Test()
+        {
+            Console.WriteLine("Test () method in point class");
+            Console.WriteLine("Created a new point object variable");
+
+        }
+    }
+    class program : point
+    {
+        static void Main()
+        {
+            point p = new point();
+            program p1 = new program();
+            p1.Test();
+            //p.Test();
+        }
+    }
+    namespace N
+    {
+        class sample
+        {
+            public void Display()
+            {
+                C.point p = new C.point();
+
+            }
+        }
+
+    }
+} 
+
