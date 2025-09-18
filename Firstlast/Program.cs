3647a3406df47565919a240dfab6fdb061b50642
mvishwasnayak-web <mvishwasnayak@gmail.com>
2025-09-18 10:32:35 +0530

    Add files via upload

---
+++ Firstlast/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       Console.WriteLine("Enter a number");
+       int num = Convert.ToInt32(Console.ReadLine());
+       int last = num % 10;
+         int first = num;
+         while (first >= 10)
+            {
+                first /= 10;
+        }
+         Console.WriteLine("First digit: " + first);
+        Console.WriteLine("Last digit: " + last);
+        Console.ReadLine();
+    }
+  
+}
