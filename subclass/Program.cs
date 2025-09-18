3b6f3f0dffc6ca28f7a43643e5ee29b01ed3c118
mvishwasnayak-web <mvishwasnayak@gmail.com>
2025-09-18 10:37:32 +0530

    Add files via upload

---
+++ subclass/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+class shape
+{
+    public virtual void Draw()
+    {
+        Console.WriteLine("Drawing a shape");
+    }
+    public virtual void Erase()
+    {
+        Console.WriteLine("Erasing the shape");
+    }
+}
+class circle : shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a circle");
+    }
+    public override void Erase()
+    {
+        Console.WriteLine("Erasing the shape");
+    }
+}
+class triangle : shape
+{
+     public override void Draw()
+    {
+        Console.WriteLine("Drawing a triangle");
+    }
+    public override void Erase()
+    {
+        Console.WriteLine("Erasing the shape");
+    }
+}
+class program
+{
+    static void Main()
+    {
+        shape[] shape = (new circle(), new triangle());
+        foreach (shape s in shapes)
+        {
+            s.Draw();
+            s.Erase();
+            Console.WriteLine();
+        }
+        
+    }
+    
+}
