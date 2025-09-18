0471ab05d5d8e32df8e45afa0fecd3f670bf1968
mvishwasnayak-web <mvishwasnayak@gmail.com>
2025-09-18 10:51:31 +0530

    Add files via upload

---
+++ vehicle.cs/vehicle.cs/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public interface IVehicle
+{
+    void Start();
+    void Stop();
+}
+
+public class Car : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("The car is starting...");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("The car is stopping...");
+    }
+}
+
+public class Truck : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("The truck is starting...");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("The truck is stopping...");
+    }
+}
+
+public class Bike : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("The bike is starting...");
+    }
+    public void Stop()
+    {
+        Console.WriteLine("The bike is stopping...");
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        IVehicle myCar = new Car();
+        IVehicle myTruck = new Truck();
+        IVehicle myBike = new Bike();
+
+        Console.WriteLine("Demonstrating car:");
+        myCar.Start();
+        myCar.Stop();
+
+        Console.WriteLine("\nDemonstrating truck:");
+        myTruck.Start();
+        myTruck.Stop();
+
+        Console.WriteLine("\nDemonstrating bike:");
+        myBike.Start();
+        myBike.Stop();
+    }
+}
