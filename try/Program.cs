9c8c5e2cc8469318816981499ba14533a227e340
mvishwasnayak-web <mvishwasnayak@gmail.com>
2025-09-18 10:42:59 +0530

    Add files via upload

---
+++ try/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("enter the first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter the second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result = num1 / num2;
+            Console.WriteLine("The result is: " + result);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: Division by zero is not allowed.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("An unexpected error occurred: ");
+        }
+        finally
+        {
+            Console.WriteLine("End of the program:");
+        }
+        Console.ReadLine();
+    }
+}
