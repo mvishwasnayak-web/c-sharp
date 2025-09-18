5328d45ddac6dba02101f88b5604e27b3aef17e3
mvishwasnayak-web <mvishwasnayak@gmail.com>
2025-09-18 10:41:12 +0530

    Add files via upload

---
+++ sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+        while (num != 0)
+        {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        Console.WriteLine(sum);
+        Console.ReadLine();
+    }
+}
+
+
+
+
+
+
+
