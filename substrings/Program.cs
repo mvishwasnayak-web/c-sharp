9a0c62b41d568dea59d631cdd6fe71c7937f1f5e
mvishwasnayak-web <mvishwasnayak@gmail.com>
2025-09-18 10:40:05 +0530

    Add files via upload

---
+++ substrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace substrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine() ?? string.Empty;
+
+            Console.WriteLine("All substrings:");
+            for (int start = 0; start < input.Length; start++)
+            {
+                for (int length = 1; length <= input.Length - start; length++)
+                {
+                    string substring = input.Substring(start, length);
+                    Console.WriteLine(substring);
+                }
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
