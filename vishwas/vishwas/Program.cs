ae7933541a1cca570e1d79044b1fbaddcde3c832
mvishwasnayak-web <mvishwasnayak@gmail.com>
2025-09-18 10:52:35 +0530

    Add files via upload

---
+++ vishwas/vishwas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Transactions;
+class program
+{ 
+    static void Main()
+    {
+        Console.WriteLine("Enter the number:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int sum = 0, Temp = num;
+        while (Temp > 0)
+        {
+            int digit = Temp % 10;
+            sum += digit * digit * digit;
+            Temp /= 10;
+                }
+        if (num == sum)
+        {
+            Console.WriteLine("AMSTROM");
+        }
+        else
+        {
+            Console.WriteLine("NOT AMSTROM");
+        }
+
+
+
+        }
+
+
+        }
