d1bae2c07e03fd56dda67e57f83eb61db1f16f36
mvishwasnayak-web <mvishwasnayak@gmail.com>
2025-09-18 10:25:49 +0530

    Add files via upload

---
+++ Amstrom/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main()
+        {
+         int num, a, b, c, d;
+         for(int i = 1; i <= 1000; i++) 
+        {
+            num = i;
+            a = num % 10;
+            b = (num / 10) % 10; 
+            c = (num / 100) % 10;
+            d = (num / 1000) % 10;
+            if (a * a * a + b * b * b + c * c * c + d * d * d == num)
+            {
+                Console.WriteLine(num);
+            }
+        }
+         Console.ReadLine();
+    }
+}
+
+
+
+
